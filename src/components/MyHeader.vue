05debb44ec2316e1b718c65e340e783fc5500c5c
MattiaMessana <mattia.messana@icloud.com>
2024-05-14 11:44:32 +0200

    add: MyHeader and body font

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -2,6 +2,8 @@
 export default {
 	data() {
 		return {
+            logoTitle: "Cinematico-logo",
+            imgLogo: "otherLogo.jpg",
 			menu: [
 				{
 					title: "Home",
@@ -16,25 +18,44 @@ export default {
 					routeName: "contacts",
 				},
 			],
+
 		};
 	},
+    methods: {
+            getImagePath(imageName) {
+                return new URL(`../assets/img/${imageName}`, import.meta.url).href;
+            },
+        },
 };
 </script>
 
 <template>
-	<header>
-		<ul>
-			<li v-for="item in menu">
+	<header class="d-flex flex-row justify-content-around text-center align-items-center">
+        <div>
+            <img :src="getImagePath(imgLogo)" :alt="logoTitle">
+        </div>
+		<ul class="d-flex">
+			<li v-for="item in menu" class="mx-2">
 				<router-link :to="{ name: item.routeName }" class="nav-link">
 					{{ item.title }}
 				</router-link>
 			</li>
 		</ul>
+        <div>
+            <ul class="d-flex">
+                <li class="mx-2"><i class="fa-brands fa-facebook-f"></i></li>
+                <li class="mx-2"><i class="fa-brands fa-youtube"></i></li>
+                <li class="mx-2"><i class="fa-brands fa-instagram"></i></li>
+            </ul>
+        </div>
 	</header>
 </template>
 
 <style scoped lang="scss">
-header {
-    height: 80px;
-}
+    header {
+        height: 80px;
+        img {
+            width: 70px;
+        }
+    }
 </style>
