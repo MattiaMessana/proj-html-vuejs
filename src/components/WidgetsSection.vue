f975cfde4a01a1e0d46a87f0308b7d87988164fb
MattiaMessana <mattia.messana@icloud.com>
2024-05-15 14:50:48 +0200

    add: WidgetsSection component

---
+++ src/components/WidgetsSection.vue
@@ -0,0 +1,37 @@
+<script>
+    export default {
+        data() {
+            return {
+                widgets: ["01", "02", "03", "04"],
+            };
+        },
+        methods: {
+		getPathImg(fileName) {
+			return new URL(`../assets/img/${fileName}.png`, import.meta.url).href;
+		},
+	},
+    }
+</script>
+
+<template>
+    
+    <section class="widget-container py-7">
+            <div class="container">
+                <div class="row">
+                    <div v-for="widget in widgets" class="col-3 d-flex justify-content-center">
+                        <img class="w-75" :src="getPathImg(widget)" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    
+</template>
+
+<style lang="scss" scoped>
+  .widget-container {
+        background-image: url(https://wordpressthemes.live/WP02/WP031/wp-content/uploads/2022/08/Parallax-03.jpg);
+        background-position: center;
+        background-size: cover;
+    }
+
+</style>
--- src/pages/MyAbout.vue
+++ src/pages/MyAbout.vue
@@ -1,11 +1,17 @@
 <script>
+import AboutSection from '../components/AboutSection.vue';
 import BestHitsCard from '../components/BestHitsCard.vue';
 import CardListMember from '../components/CardListMember.vue';
+import WidgetsSection from '../components/WidgetsSection.vue';
+
 
     export default {
         components: {
             BestHitsCard,
             CardListMember,
+            AboutSection,
+            WidgetsSection,
+            
         },
         data() {
             return {
@@ -27,7 +33,6 @@ import CardListMember from '../components/CardListMember.vue';
 					type: "Music Video",
 				},
 			],
-            widgets: ["01", "02", "03", "04"],
             };
         },
         methods: {
@@ -40,53 +45,57 @@ import CardListMember from '../components/CardListMember.vue';
 
 <template>
 
-    <div class="bg-image">
-        <div class="contacts">
-            <h2>ABOUT</h2>
+    <!-- hero -->
+
+        <div class="bg-image">
+            <div class="contacts">
+                <h2>ABOUT</h2>
                 <div class="home-contacts">
                     <router-link class="home" :to="{ name: 'home' }">Home</router-link>
                     <span> / About</span>
                 </div>
-        </div>
-    </div>
-
-       
-    <div class="cards row px-5 text-center mb-5 mt-3">
-        <h5 class="mt-4">YOUR WATCHLIST</h5>
-        <h2 class="mb-5">BEST HITS MOVIES</h2>
-        <BestHitsCard class="col-4" v-for="card in bestHits" :card="card" />
-    </div>
-
-    
-    <div class="MySection d-flex align-items-center">
-        <div class="container"></div>
-        <div class="container p-4">
-            <span>ABOUT US</span>
-            <h3 class="my-3">HIGH QUALITY CINEMATIC</h3>
-            <p>
-                Orci varius penatibus et magnis dis parturient In the montes, nascetur ridiculus mus. Nulla
-                A nisi ut mi efficitur hendrerit. nunc urna. quisque a partr hendrerit purus erat, nec dapibus
-                sem sed. phasellus sed eli molestie, porttitor ligula of the egestas, mattis augue. quisque et
-                rhoncus justo. suspendisse luctus pharetra eleme tcondimentum.
-            </p>
-            <button class="btn btn-light rounded-0 px-4">READ MORE</button>
+            </div>
         </div>
         
-    </div>
+    <!-- /hero -->
 
-    <section class="my-6 container text-center">
-		<CardListMember />
-	</section>
-       
-        <section class="widget-container py-7">
-			<div class="container">
-				<div class="row">
-					<div v-for="widget in widgets" class="col-3 d-flex justify-content-center">
-						<img class="w-75" :src="getPathImg(widget)" alt="" />
-					</div>
-				</div>
-			</div>
+    
+
+
+    <!-- BestHitsCard  -->
+
+        <div class="cards row px-5 text-center mb-5 mt-3">
+            <h5 class="mt-4">YOUR WATCHLIST</h5>
+            <h2 class="mb-5">BEST HITS MOVIES</h2>
+            <BestHitsCard class="col-4" v-for="card in bestHits" :card="card" />
+        </div>
+
+    <!-- /BestHitsCard  -->
+
+
+
+    <!-- AboutSection -->
+
+        <AboutSection />
+
+    <!-- /AboutSection -->
+
+
+    
+    <!-- CardListMember -->
+
+        <section class="my-6 container text-center">
+            <CardListMember />
         </section>
+
+    <!-- /CardListMember -->
+
+    <!-- WidgetsSection -->
+
+        <WidgetsSection />
+
+    <!-- WidgetsSection -->
+
        
 </template>
 
@@ -117,16 +126,6 @@ import CardListMember from '../components/CardListMember.vue';
         }
     }
 }
-    .MySection {
-        color: white;
-        background-color: black;
-        // debug
-        min-height: 500px;
-    }
     
-    .widget-container {
-	    background-color: black;
-    }
-
 
 </style>
