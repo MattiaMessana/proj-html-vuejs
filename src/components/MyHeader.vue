82d08cfdcae58ed19e93dec8a8f089dc22c11ea7
MattiaMessana <mattia.messana@icloud.com>
2024-05-15 17:10:59 +0200

    add: zoom effect in MyHeader

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -33,21 +33,24 @@ export default {
 	<header class="d-flex flex-row justify-content-between  p-3">
 		
         <div>
-            <img :src="getImagePath(imgLogo)" :alt="logoTitle" class="align-self-center">
+			<router-link class="home" :to="{ name: 'home' }">
+				<img :src="getImagePath(imgLogo)" :alt="logoTitle" class="align-self-center">
+			</router-link>
+
         </div>
 
 		<ul class="d-flex">
 			<li v-for="item in menu" class="mx-2 align-self-center">
-				<router-link :to="{ name: item.routeName }" class="nav-link">
+				<router-link :to="{ name: item.routeName }" class="nav-link zoom">
 					{{ item.title }}
 				</router-link>
 			</li>
 		</ul>
         <div>
             <ul class="d-flex">
-                <li class="mx-2"><i class="fa-brands fa-facebook-f"></i></li>
-                <li class="mx-2"><i class="fa-brands fa-youtube"></i></li>
-                <li class="mx-2"><i class="fa-brands fa-instagram"></i></li>
+                <li class="mx-2 zoom"><i class="fa-brands fa-facebook-f"></i></li>
+                <li class="mx-2 zoom"><i class="fa-brands fa-youtube"></i></li>
+                <li class="mx-2 zoom"><i class="fa-brands fa-instagram"></i></li>
             </ul>
         </div>
 	</header>
@@ -66,4 +69,11 @@ export default {
             width: 150px;
         }
     }
+	.zoom {
+	padding: 5px;
+	transition: transform .2s; /* Animation */
+	}
+	.zoom:hover {
+	transform: scale(1.3);
+	}
 </style>
